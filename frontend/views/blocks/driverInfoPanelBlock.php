5335b26224ed6cfe7de19c7353741e08270ff1e3
Pottie <git@pottie.com>
2015-07-15 11:24:38 +1000

    Faked Driver Image
    Thumbnail missing from image class. Replaced with temporary hardcode.

--- frontend/views/blocks/driverInfoPanelBlock.php
+++ frontend/views/blocks/driverInfoPanelBlock.php
@@ -8,6 +8,6 @@
     </div>
 </div>
 <div class="user-photo-container f-left">
-    <img src="<?= $driver->image ? $driver->image->thumblUrl : '/img/temp/01.jpg' ?>" 
-        alt="<?= $driver->username ?>" title="<?= $driver->username ?>" />
+<!--    <img src="<?//= $driver->image ? $driver->image->thumbnail : '/img/temp/01.jpg' ?>" -->
+   <img src="<?= $driver->image ? '/img/temp/01.jpg' : '/img/temp/01.jpg' ?>"  alt="<?= $driver->username ?>" title="<?= $driver->username ?>" />
 </div>
