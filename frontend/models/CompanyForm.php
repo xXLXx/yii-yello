cfe1e29a81df16d419da3e8e9eef2ca789b3cd3b
jovani <jovani@bywave.com.au>
2015-08-04 14:15:19 +0800

    Fixed. CompanyForm not saving for first signup.

--- frontend/models/CompanyForm.php
+++ frontend/models/CompanyForm.php
@@ -103,7 +103,7 @@ class CompanyForm extends Model
         $transaction = \Yii::$app->db->beginTransaction();
 
         try {
-            $company = Company::findOne($this->id);
+            $company = Company::findOneOrCreate($this->id);
             $company->ABN = $this->ABN;
             $company->accountName = $this->accountName;
             $company->companyName = $this->companyName;
@@ -115,6 +115,8 @@ class CompanyForm extends Model
 
                 throw new \yii\db\Exception(current($error));
             }
+
+            $this->id = $company->id;
 
             $companyAddress = CompanyAddress::findOneOrCreate(array(
                 'companyfk' => $this->id,
@@ -143,6 +145,7 @@ class CompanyForm extends Model
             }
 
             $companyAddress->addressfk = $address->idaddress;
+            $companyAddress->companyfk = $this->id;
             $companyAddress->contact_name = $this->contact_name;
             $companyAddress->contact_phone = $this->contact_phone;
             $companyAddress->contact_email = $this->contact_email;
