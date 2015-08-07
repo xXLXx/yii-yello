592205a6d6e25e87a81c8eabb89ef5015f84d59c
jovani <jovani@bywave.com.au>
2015-08-07 13:03:23 +0800

    Refactored DriverForm with transactions.

--- frontend/models/UserForm/DriverForm.php
+++ frontend/models/UserForm/DriverForm.php
@@ -21,10 +21,6 @@ class DriverForm extends UserForm
     public $userId;
     public $company;
     public $abn;
-    
-    
-    
-    
 
     /**
      * @inheritdoc
@@ -50,94 +46,133 @@ class DriverForm extends UserForm
     }
 
     /**
+     * Saves driver personal info.
+     * 1. Create UserDriver, if one not yet available
+     * 2. Create Vehicle, if one not yet available
+     * 3. Save imageFile uploaded
+     * 4. Create company and its address
+     *
      * @inheritdoc
      */
     public function save()
     {
-        $fullname = (string)  $this->firstName.' '.(string)$this->lastName;
-        file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export('savePersonal' . PHP_EOL, true), FILE_APPEND);
-        $user = \Yii::$app->user->identity;
-        $userDriver = UserDriver::findOne(['userId' => $user->id]);
-        if (!$userDriver) {
-            $userDriver = new UserDriver();
-        }
-        $vehicle = Vehicle::findOne(['driverId' => $user->id]);
-        if (!$vehicle) {
-            $vehicle = new Vehicle();
-        }
-        
-        file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export('imagePersonal' . PHP_EOL, true), FILE_APPEND);
-        if (isset($_FILES['imageFile'])) {
-            $image = new Image();
-            $image->imageFile = UploadedFile::getInstanceByName('imageFile');
-            if ($image->imageFile) {
-                $image->save();
-                $image->saveFiles();
-                $image->save();
-                $user->imageId = $image->id;
-                file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export($image->imageFile, true), FILE_APPEND);
+        $transaction = \Yii::$app->db->beginTransaction();
+
+        try {
+            $fullname = (string)  $this->firstName.' '.(string)$this->lastName;
+            file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export('savePersonal' . PHP_EOL, true), FILE_APPEND);
+            $user = \Yii::$app->user->identity;
+            $userDriver = UserDriver::findOneOrCreate(['userId' => $user->id]);
+            $vehicle = Vehicle::findOneOrCreate(['driverId' => $user->id]);
+
+            file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export('imagePersonal' . PHP_EOL, true), FILE_APPEND);
+            if (isset($_FILES['imageFile'])) {
+                $image = new Image();
+                $image->imageFile = UploadedFile::getInstanceByName('imageFile');
+                if ($image->imageFile) {
+                    $image->save();
+                    $image->saveFiles();
+                    $image->save();
+                    $user->imageId = $image->id;
+                    file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export($image->imageFile, true), FILE_APPEND);
+                }
             }
+
+            $user->firstName = $this->firstName;
+            $user->lastName = $this->lastName;
+            $user->signup_step_completed = 1;
+            if (!$user->save()) {
+                $error = $user->getFirstError();
+                $this->addError(key($error), current($error));
+
+                throw new \yii\db\Exception(current($error));
+            }
+
+            $company = Company::findOneOrCreate(['userfk' =>$user->id, 'isPrimary' => 1]);
+            if ($company->isNewRecord) {
+                $company->accountName=$this->firstName.' '.$this->lastName;
+                $company->email=$user->email;
+            }
+
+            // add /update company
+            $company->registeredForGST=0;
+            $company->companyName=$this->company;
+            $company->ABN=$this->abn;
+            if (!$company->save()) {
+                $error = $company->getFirstError();
+                $this->addError(key($error), current($error));
+
+                throw new \yii\db\Exception(current($error));
+            }
+
+            $companyaddress = CompanyAddress::findOneOrCreate(['companyfk' => $company->id , 'addresstitle' => 'Default']);
+            if ($companyaddress->isNewRecord) {
+                $companyaddress->addresstype = 1;
+                $companyaddress->contact_name = $fullname;
+                $companyaddress->contact_email = $user->email;
+
+                $address = new Address();
+                $address->setAttributes($this->getAttributes());
+                if (!$address->save()) {
+                    $error = $address->getFirstError();
+                    $this->addError(key($error), current($error));
+
+                    throw new \yii\db\Exception(current($error));
+                }
+            } else {
+                $address = Address::findOneOrCreate(['idaddress' => $companyaddress->idcompanyaddress]);
+                $address->setAttributes($this->getAttributes());
+                if (!$address->save()) {
+                    $error = $address->getFirstError();
+                    $this->addError(key($error), current($error));
+
+                    throw new \yii\db\Exception(current($error));
+                }
+            }
+
+            $companyaddress->addressfk = $address->idaddress;
+            if (!$companyaddress->save()) {
+                $error = $companyaddress->getFirstError();
+                $this->addError(key($error), current($error));
+
+                throw new \yii\db\Exception(current($error));
+            }
+
+            // @todo: jovani - add logic to figure out timezone and currency
+
+            file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export($user->toArray(), true), FILE_APPEND);
+            $userDriver->setAttributes($this->getAttributes());
+
+            $userDriver->userId = $user->id;
+            if (!$userDriver->save()) {
+                $error = $userDriver->getFirstError();
+                $this->addError(key($error), current($error));
+
+                throw new \yii\db\Exception(current($error));
+            }
+
+            file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export($userDriver->getErrors(), true), FILE_APPEND);
+            file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export($vehicle->toArray(), true), FILE_APPEND);
+            $this->image = $user->image;
+            $this->userId = $user->id;
+
+            $vehicle->driverId = $user->id;
+            $vehicle->setAttributes($this->getAttributes());
+            if (!$vehicle->save()) {
+                $error = $vehicle->getFirstError();
+                $this->addError(key($error), current($error));
+
+                throw new \yii\db\Exception(current($error));
+            }
+
+            $transaction->commit();
+
+            return true;
+        } catch (\Exception $e) {
+            \Yii::error($e->getMessage());
+            $transaction->rollBack();
         }
 
-        
-        $user->firstName = $this->firstName;
-        $user->lastName = $this->lastName;
-        $user->save();
-        
-
-        $companyobj = Company::findOne(['userfk'=>$user->id, 'isPrimary'=>1]);
-        if(!$companyobj){
-            $companyobj = new Company();
-            $companyobj->userfk = $user->id;
-            $companyobj->isPrimary=1;
-            $companyobj->accountName=$this->firstName.' '.$this->lastName;
-            $companyobj->email=$user->email;
-        }
-        
-
-        // add /update company
-        $companyobj->registeredForGST=0;
-        $companyobj->companyName=$this->company;
-        $companyobj->ABN=$this->abn;
-        $companyobj->save();
-        
-        $companyaddress = CompanyAddress::findOne(['companyfk'=>$companyobj->id , 'addresstitle'=>'Default']);
-        if(!$companyaddress){
-            $companyaddress = new CompanyAddress();
-            $companyaddress->companyfk=$companyobj->id;
-            $companyaddress->addresstype=1;
-            $companyaddress->addresstitle='Default';
-            $companyaddress->contact_name=$fullname;
-            $companyaddress->contact_email=$user->email;
-        }
-        $companyaddress->save();
-        
-        $address= Address::findOne(['idaddress'=>$companyaddress->idcompanyaddress]);
-        if(!$address){
-            $address = new Address();
-        }
-        $address->save();
-        $companyaddress->addressfk=$address->idaddress;
-        $companyaddress->save();
-        $address->setAttributes($this->getattributes());
-        $address->save();
-        // todo: jovani - add logic to figure out timezone and currency
-        
-        file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export($user->toArray(), true), FILE_APPEND);
-        $userDriver->setAttributes($this->getAttributes());
-
-        $userDriver->userId = $user->id;
-        $userDriver->save();
-        file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export($userDriver->getErrors(), true), FILE_APPEND);
-        $vehicle->driverId = $user->id;
-        $vehicle->setAttributes($this->getAttributes());
-        $vehicle->save();
-        file_put_contents(\Yii::$app->basePath . '/../frontend/runtime/logs/driverApiLog.txt', var_export($vehicle->toArray(), true), FILE_APPEND);
-        $this->image = $user->image;
-        $this->userId = $user->id;
-
-        $user->signup_step_completed = 1;
-        $user->save();
-
+        return false;
     }
 }
