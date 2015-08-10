d94b9229a53c88cce0179999fe0a038f5bf9b87f
xXLXx <leo.lavarias>
2015-08-10 15:37:20 +0800

    [#100634154] Fix Pjax Bug for shift view, edit, and add

--- frontend/widgets/ShiftForm/views/default.php
+++ frontend/widgets/ShiftForm/views/default.php
@@ -14,17 +14,18 @@ $title = $model->id ? 'Edit shifts' : 'Add Shifts';
 
 ?>
 
-<?php 
+<?php
     Pjax::begin([
         'id' => "shift-form-widget-pjax",
         'enablePushState' => true,
-        'linkSelector'    => '.js-pjax'
-    ]); 
+        'linkSelector'    => '.js-pjax',
+        'timeout'         => 30000
+    ]);
 ?>
 
 <h2><?= \Yii::t('app', $title) ?></h2>
 <div class="assigned-shifts-filter <?php if($model->driverId): ?>allocate<?php endif; ?>">
-    <?php 
+    <?php
         $form = ActiveForm::begin([
             'layout' => 'horizontal',
             'id' => 'shift-form',
@@ -38,14 +39,14 @@ $title = $model->id ? 'Edit shifts' : 'Add Shifts';
                 'enctype'   => 'multipart/form-data',
                 'data-pjax' => 1
             ]
-        ]); 
+        ]);
     ?>
         <?= Html::activeHiddenInput($model, 'id'); ?>
         <?= Html::activeHiddenInput($model, 'storeId'); ?>
         <?= Html::errorSummary($model); ?>
         <div class="input-wrapp date">
             <div class="text-block"><?= \Yii::t('app', 'Date') ?></div>
-            <?= 
+            <?=
                 DatePicker::widget([
                     'model'         => $model,
                     'attribute'     => 'date',
@@ -59,7 +60,7 @@ $title = $model->id ? 'Edit shifts' : 'Add Shifts';
         </div>
         <div class="input-wrapp time">
             <div class="text-block"><?= \Yii::t('app', 'Time') ?></div>
-            <?= 
+            <?=
                 TimepickerWidget::widget([
                     'name'  => $model->formName() . '[start]',
                     'value' => $model->start,
@@ -69,7 +70,7 @@ $title = $model->id ? 'Edit shifts' : 'Add Shifts';
                     ]
                 ]);
             ?>
-            <?= 
+            <?=
                 TimepickerWidget::widget([
                     'name'  => $model->formName() . '[end]',
                     'value' => $model->end,
@@ -86,7 +87,7 @@ $title = $model->id ? 'Edit shifts' : 'Add Shifts';
             <label class="bold-text"><?= \Yii::t('app', 'Vehicle') ?></label>
             <div class="j_radio_container">
                 <div class="radio-input">
-                    <input id="vehicle-1" name="<?= $model->formName(); ?>[isVehicleProvided]" value=0 
+                    <input id="vehicle-1" name="<?= $model->formName(); ?>[isVehicleProvided]" value=0
                         type="radio" <?php if (!$model->isVehicleProvided): ?>checked="checked"<?php endif; ?> >
                     <label class="j_radio <?php if (!$model->isVehicleProvided): ?>active<?php endif; ?>" for="vehicle-1"><?= \Yii::t('app', 'Driver Owned'); ?></label>
                 </div>
@@ -101,44 +102,44 @@ $title = $model->id ? 'Edit shifts' : 'Add Shifts';
             <label class="bold-text"><?= \Yii::t('app', 'Driver') ?></label>
             <div class="j_checkbox_container js-driver-visible-group-container">
                 <div class="checkbox-input">
-                    <input class="js-driver-group-radio" 
+                    <input class="js-driver-group-radio"
                         id="driver-1" name="<?= $model->formName() . '[visibleGroup][]' ?>" value="isYelloDrivers"
                         type="checkbox" <?php if ($model->isYelloDrivers): ?>checked="checked"<?php endif; ?> >
                     <label class="j_checkbox <?php if ($model->isYelloDrivers): ?>active<?php endif; ?>" for="driver-1"><?= \Yii::t('app', 'Yello'); ?></label>
                 </div>
                 <div class="checkbox-input">
-                    <input class="js-driver-group-radio" 
+                    <input class="js-driver-group-radio"
                         id="driver-2" name="<?= $model->formName() . '[visibleGroup][]' ?>" value="isFavourites"
                         type="checkbox" <?php if ($model->isFavourites): ?>checked="checked"<?php endif; ?> >
                     <label class="j_checkbox <?php if ($model->isFavourites): ?>active<?php endif; ?>" for="driver-2"><?= \Yii::t('app', 'Favourites'); ?></label>
                 </div>
                 <div class="checkbox-input">
-                    <input class="js-driver-group-radio" 
+                    <input class="js-driver-group-radio"
                         id="driver-3" name="<?= $model->formName() . '[visibleGroup][]' ?>" value="isMyDrivers"
                         type="checkbox" <?php if ($model->isMyDrivers): ?>checked="checked"<?php endif; ?> >
                     <label class="j_checkbox <?php if ($model->isMyDrivers): ?>active<?php endif; ?>" for="driver-3"><?= \Yii::t('app', 'My Driver(s)'); ?></label>
                 </div>
             </div>
         </div>
-        
+
         <div class="js-driver-search-container hide">
-            <?= 
+            <?=
                 DriverSearchWidget::widget([
                     'model' => $model
                 ]);
             ?>
         </div>
-        
+
         <div class="border-top-block">
-            <?= 
+            <?=
                 Html::submitButton(\Yii::t('app', 'Post Shift'), [
                     'class' => 'btn blue post-submit'
-                ]); 
+                ]);
             ?>
-            <?= 
+            <?=
                 Html::submitButton(\Yii::t('app', 'Allocate Shift'), [
                     'class' => 'btn blue allocate-submit'
-                ]); 
+                ]);
             ?>
         </div>
     <?php ActiveForm::end(); ?>
