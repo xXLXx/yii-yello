b14842546af5cae4447f4074f76045bf846547d3
vsmash <mark@pottie.com>
2015-08-14 13:53:16 +1000

    Added dummy field to address to prevent chrome autofill.

--- frontend/widgets/Address/views/index.php
+++ frontend/widgets/Address/views/index.php
@@ -1,9 +1,12 @@
 <?php use \yii\helpers\Html; ?>
 
 <div class="row">
+    
     <div class="form-group col-sm-12">
         <?= Html::textInput($name, null, $options); ?>
     </div>
+    <?php // this will prevent chrome from overlaying with autofill ?>
+    <input autocomplete="false" name="hidden" type="text" style="display:none;"> 
     <?= $form->field($model, 'block_or_unit',['options'=>['class'=>'form-group col-sm-2'], 'inputOptions' => ['readonly' => true]])->label(false); ?>
     <?= $form->field($model, 'street_number',['options'=>['class'=>'form-group col-sm-3'], 'inputOptions' => ['readonly' => true]])->label(false); ?>
     <?= $form->field($model, 'route',['options'=>['class'=>'form-group col-sm-7'], 'inputOptions' => ['readonly' => true]])->label(false); ?>
