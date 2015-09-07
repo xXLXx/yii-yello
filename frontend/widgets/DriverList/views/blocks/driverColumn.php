65ff4fc826c051d132422a139158fffa59f252bc
jovani <jovani@bywave.com.au>
2015-09-07 09:11:13 +0800

    Applied image handler on drivers page.

--- frontend/widgets/DriverList/views/blocks/driverColumn.php
+++ frontend/widgets/DriverList/views/blocks/driverColumn.php
@@ -4,8 +4,8 @@ use yii\helpers\Html;
 <div class="driver-info">
     <div class="user-photo-container f-left">
         <a href="/drivers/profile?id=<?=$driver->id?>"><img
-                src="<?= $driver->image ? $driver->image->thumbUrl : '/img/Driver_Pic_bgrey_black.png' ?>"
-                alt="<?= $driver->image ? $driver->image->alt : '' ?>"/>
+                src="/images/profile-thumb/<?= $driver->id; ?>"
+                alt="<?= $driver->username; ?>"/>
         </a>
     </div>
     <div class="user-panel-button">
