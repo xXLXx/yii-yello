04858fbf883adb109bcb03606871dbf41c3da668
Lalit J <lalit@bywave.com.au>
2015-09-08 10:27:48 +0530

    removed bootstrap file from plugin bundle.

--- vendor/kartik-v/yii2-krajee-base/AssetBundle.php
+++ vendor/kartik-v/yii2-krajee-base/AssetBundle.php
@@ -28,7 +28,7 @@ class AssetBundle extends \yii\web\AssetBundle
     public $depends = [
         'yii\web\JqueryAsset',
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        //'yii\bootstrap\BootstrapAsset',
     ];
 
     /**
