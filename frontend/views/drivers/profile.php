dba0cac87132bd9f69e4854e55859cf5bfc09567
Lalit J <lalit@bywave.com.au>
2015-09-10 12:28:36 +0530

    Star rating local widget. Init addFav on roster page. Added no shifts/text on Shifts Assigned page.

--- frontend/views/drivers/profile.php
+++ frontend/views/drivers/profile.php
@@ -46,7 +46,7 @@ $driverId = $driver->id;
                             <div>
 
 
-                                <?php echo \kartik\rating\StarRating::widget(['value' => $review_avg]); ?>
+                                <?php echo \frontend\widgets\StarRating\StarRating::widget(['value' => $review_avg]); ?>
                                 <!--<span class="star-block">
                                     <span class="font-star-two"></span>
                                     <span class="font-star-two"></span>
@@ -191,7 +191,7 @@ $driverId = $driver->id;
                             <?php foreach ($reviews as $review):?>
                                 <div class="company-item">
                                     <h5><?= $review->store->title; ?></h5>
-                                    <div><?php echo \kartik\rating\StarRating::widget(['value' => $review->stars]); ?></div>
+                                    <div><?php echo \frontend\widgets\StarRating\StarRating::widget(['value' => $review->stars]); ?></div>
                                     <div><?= $review->text; ?></div>
                                     <div class="gray-text">
                                         <?php
