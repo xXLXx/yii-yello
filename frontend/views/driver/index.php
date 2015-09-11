adbf0c49c195f202d8b93b6d013fa17bc5d8641b
Mark <mark@driveyello.com>
2015-09-11 02:17:28 +0000

    change to simple layout and styles

--- frontend/views/driver/index.php
+++ frontend/views/driver/index.php
@@ -4,11 +4,11 @@ use yii\helpers\Html;
 $this->title = 'Driver Info / Yello';
 ?>
 <div class="login-container-inner">
-    <div class="login-popup width-340">
+    <div class="login-popup width-266">
         <div class="select-form">
             <form action="">
                 <h2 class="center"><?= \Yii::t('app', 'Driver Website Coming Soon'); ?></h2>
-                <p>Thank you for visiting us. Your exclusive driver's website is on its way. Meanwhile you may like to download the app or subscribe using these buttons.</p>
+                <p style="text-align: justify;">Thank you for visiting us. Your exclusive driver's website is on its way. Meanwhile you may like to download the app or subscribe using these buttons.</p>
                 <?= Html::a(\Yii::t('app', 'Yello Website'), ['https://www.driveyello.com/registration/'], ['class' => 'btn blue']); ?>
                 <?= Html::a(\Yii::t('app', 'Download the App'), ['#'], ['class' => 'btn blue']); ?>
             </form>
