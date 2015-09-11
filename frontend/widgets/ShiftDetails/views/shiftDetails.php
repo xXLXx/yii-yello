5e591277782e6ca21294a7b79e11118e0806a07c
Alireza Norouzi <alireza@Ali-Mac-mini.netgear.com>
2015-09-11 17:26:29 +1000

    placeholder and default star for review fixed.

--- frontend/widgets/ShiftDetails/views/shiftDetails.php
+++ frontend/widgets/ShiftDetails/views/shiftDetails.php
@@ -149,8 +149,8 @@
         <?php if( $shiftState->name === $shiftState::STATE_APPROVAL || $shiftState->name === $shiftState::STATE_DISPUTED || $shiftState->name === $shiftState::STATE_UNDER_REVIEW): ?>
             <h4 class="inline-block valign-middle">Rate Driver</h4>
 
-            <?php echo \kartik\rating\StarRating::widget(['value' => 3, 'disabled' => false]); ?>
-            <div><textarea class="textarea j_driver-review" >Nice job. as always! Thank you!</textarea></div>
+            <?php echo \kartik\rating\StarRating::widget(['value' => 0, 'disabled' => false]); ?>
+            <div><textarea class="textarea j_driver-review" placeholder="Write a review for driver!"></textarea></div>
 
             <div class="button-container j_request_link">
 
